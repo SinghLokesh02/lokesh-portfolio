0ee1c6fe760e777df0c38e268c1db3cae5f27d6b
Lokesh <lokeshsingh7695@gmail.com>
2025-01-08 18:11:07 +0530

    Added Proper Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,116 +1,123 @@
 import React, { useState } from "react";
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
     <div>
-      {/* Navbar */}
-      <nav className="fixed top-0 left-0 w-full bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 shadow-lg z-50">
+      <nav className="bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-white shadow-lg w-full fixed top-0 z-30">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            {/* Logo */}
-            <div className="flex-shrink-0">
-              <h1 className="text-2xl font-bold text-purple-400">Lokesh</h1>
-            </div>
-            {/* Desktop Menu */}
-            <div className="hidden md:flex space-x-6">
-              <a
-                href="#home"
-                className="text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
-              >
-                Home
-              </a>
-              <a
-                href="#about"
-                className="text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
-              >
-                About
-              </a>
-              <a
-                href="#experience"
-                className="text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
-              >
-                Experience
-              </a>
-              <a
-                href="#skills"
-                className="text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
-              >
-                Skills
-              </a>
-              <a
-                href="#contact"
-                className="text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
-              >
-                Contact Me
-              </a>
-            </div>
-            {/* Mobile Menu Button */}
-            <div className="md:hidden">
-              <button
-                className="text-gray-300 hover:text-purple-400 focus:outline-none transition-colors duration-300"
-                onClick={toggleMenu}
-                aria-label="Toggle Menu"
-              >
-                ☰
-              </button>
+          <div className="flex justify-between h-16">
+            <div className="flex items-center justify-between w-full">
+              <div className="head">
+                <h1 className="text-2xl font-bold text-purple-400">Lokesh</h1>
+              </div>
+              <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+                <div className="others-link flex gap-x-5">
+                  <a
+                    href="#home"
+                    className="text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
+                  >
+                    Home
+                  </a>
+                  <a
+                    href="#about"
+                    className="text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
+                  >
+                    About
+                  </a>
+                  <a
+                    href="#experience"
+                    className="text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
+                  >
+                    Experience
+                  </a>
+                  <a
+                    href="#skills"
+                    className="text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
+                  >
+                    Skills
+                  </a>
+                  <a
+                    href="#contact"
+                    className="text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
+                  >
+                    Contact Me
+                  </a>
+                </div>
+              </div>
+              <div className="-mr-2 flex items-center sm:hidden">
+                <button
+                  onClick={() => setNavbarOpen(!navbarOpen)}
+                  type="button"
+                  className={`inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white duration-1000 ${
+                    navbarOpen ? "transform rotate-180" : ""
+                  }`}
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"
+                >
+                  <span className="sr-only">Open main menu</span>
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </nav>
 
-      {/* Mobile Menu */}
-      <div
-        className={`absolute top-16 left-0 w-full bg-gray-800 shadow-md transform ${
-          isMenuOpen
-            ? "translate-y-0 opacity-100"
-            : "-translate-y-full opacity-0"
-        } transition-all duration-500 ease-in-out`}
-        style={{ zIndex: 40 }}
-      >
-        <div className="py-4 px-4 space-y-4">
-          <a
-            href="#about"
-            className="block text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            Home
-          </a>
-          <a
-            href="#home"
-            className="block text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            About
-          </a>
-          <a
-            href="#experience"
-            className="block text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            Experience
-          </a>
-          <a
-            href="#skills"
-            className="block text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            Skills
-          </a>
-          <a
-            href="#contact"
-            className="block text-gray-300 hover:text-purple-400 transition-colors duration-300 text-lg"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            Contact Me
-          </a>
+        <div
+          className={`${
+            navbarOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+          } sm:hidden overflow-hidden transition-all duration-700 ease-in-out`}
+          id="mobile-menu"
+        >
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            <a
+              href="#home"
+              className="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-purple-400 hover:bg-gray-800"
+            >
+              Home
+            </a>
+            <a
+              href="#about"
+              className="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-purple-400 hover:bg-gray-800"
+            >
+              About
+            </a>
+            <a
+              href="#experience"
+              className="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-purple-400 hover:bg-gray-800"
+            >
+              Experience
+            </a>
+            <a
+              href="#skills"
+              className="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-purple-400 hover:bg-gray-800"
+            >
+              Skills
+            </a>
+            <a
+              href="#contact"
+              className="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-purple-400 hover:bg-gray-800"
+            >
+              Contact Me
+            </a>
+          </div>
         </div>
-      </div>
+      </nav>
     </div>
   );
 };
